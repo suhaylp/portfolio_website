39f4ba23abceedf7fd267a57a59797919696edbd
suhaylp <71238928+suhaylp@users.noreply.github.com>
2023-12-18 02:00:37 -0800

    Removed tutoring tab, temporarily disabled JS

--- main.js
+++ main.js
@@ -1,27 +1,27 @@
-document.addEventListener('DOMContentLoaded', () => {
-    document.querySelectorAll('nav a').forEach(link => {
-        link.addEventListener('click', (e) => {
-            e.preventDefault();
-            const targetId = link.getAttribute('href');
-            const targetElement = document.querySelector(targetId);
-            
-            if(targetElement) {
-                targetElement.scrollIntoView({ behavior: 'smooth' });
-            }
-        });
-    });
-});
+// document.addEventListener('DOMContentLoaded', () => {
+//     document.querySelectorAll('nav a').forEach(link => {
+//         link.addEventListener('click', (e) => {
+//             e.preventDefault();
+//             const targetId = link.getAttribute('href');
+//             const targetElement = document.querySelector(targetId);
 
-document.addEventListener('DOMContentLoaded', () => {
-    const navButtons = document.querySelectorAll('nav ul li a');
+//             if (targetElement) {
+//                 targetElement.scrollIntoView({ behavior: 'smooth' });
+//             }
+//         });
+//     });
+// });
 
-    navButtons.forEach(button => {
-        button.addEventListener('mouseenter', () => {
-            button.classList.add('hover-animate');
-        });
+// document.addEventListener('DOMContentLoaded', () => {
+//     const navButtons = document.querySelectorAll('nav ul li a');
 
-        button.addEventListener('mouseleave', () => {
-            button.classList.remove('hover-animate');
-        });
-    });
-});
+//     navButtons.forEach(button => {
+//         button.addEventListener('mouseenter', () => {
+//             button.classList.add('hover-animate');
+//         });
+
+//         button.addEventListener('mouseleave', () => {
+//             button.classList.remove('hover-animate');
+//         });
+//     });
+// });
